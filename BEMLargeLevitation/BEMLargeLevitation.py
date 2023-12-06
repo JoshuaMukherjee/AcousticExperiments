aa7da96b3e5ea40acab54c40ec94b8f7eeea67e0
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2023-12-06 14:32:17 +0000

    BEM torque lev

--- BEMLargeLevitation/BEMLargeLevitation.py
+++ BEMLargeLevitation/BEMLargeLevitation.py
@@ -1,12 +1,18 @@
 from BEMLevitationObjectives import BEM_levitation_objective
-from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas
-from acoustools.Utilities import TRANSDUCERS
+from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, get_lines_from_plane
+from acoustools.Utilities import TRANSDUCERS, propagate_abs
+from acoustools.BEM import compute_H, grad_H
+from acoustools.Visualiser import Visualise
+from acoustools.Solvers import gradient_descent_solver
+from acoustools.Optimise.Constraints import constrain_phase_only
 
-from acoustools.Solvers import wgs_wrapper
+from acoustools.Solvers import wgs_wrapper, wgs_batch
+
+import torch
 
 if __name__ == "__main__":
 
-    path = "Media/Sphere-lam4.stl"
+    path = "Media/Sphere-lam1.stl"
     scatterer = load_scatterer(path,dy=-0.06) #Make mesh at 0,0,0
     
     scale_to_diameter(scatterer,0.03)
@@ -19,10 +25,16 @@ if __name__ == "__main__":
 
     centres = get_centres_as_points(scatterer)
 
-    x = wgs_wrapper(centres)
+    H = compute_H(scatterer, board)
+    # Hx, Hy, Hz = grad_H(scatterer,board)
+
+    # x = wgs_wrapper(centres, A=H)
+
+    # print(propagate_abs(x,centres,A=H))
+
+    # x = wgs_wrapper(centres)
 
     norms = get_normals_as_points(scatterer)
-
     areas = get_areas(scatterer)
 
     # centres = centres.expand((2,-1,-1))
@@ -36,4 +48,15 @@ if __name__ == "__main__":
     }
 
 
-    BEM_levitation_objective(x, centres,board,**params)
+    x = gradient_descent_solver(centres, BEM_levitation_objective,constrains=constrain_phase_only,objective_params=params,log=True,iters=400,lr=1)
+
+    A = torch.tensor((-0.07,0, 0.07))
+    B = torch.tensor((0.07,0, 0.07))
+    C = torch.tensor((-0.07,0, -0.07))
+
+    origin = (0,0,-0.07)
+    normal = (0,1,0)
+
+    line_params = {"scatterer":scatterer,"origin":origin,"normal":normal}
+
+    Visualise(A,B,C,x,colour_functions=[propagate_abs], add_lines_functions=[get_lines_from_plane],add_line_args=[line_params])
--- BEMLargeLevitation/BEMLevitationObjectives.py
+++ BEMLargeLevitation/BEMLevitationObjectives.py
@@ -1,5 +1,8 @@
-from acoustools.BEM import BEM_forward_model_grad
-from acoustools.Gorkov import force_mesh
+from acoustools.BEM import BEM_forward_model_grad, grad_H
+from acoustools.Gorkov import force_mesh, torque_mesh
+from acoustools.Mesh import get_weight, get_centre_of_mass_as_points
+
+import torch
 
 def BEM_levitation_objective(transducer_phases, points, board, targets=None, **objective_params):
     ''' Expects an element of objective_params named `norms` containing all normals in the same order as points 
@@ -15,7 +18,11 @@ def BEM_levitation_objective(transducer_phases, points, board, targets=None, **o
         "scatterer":scatterer
     }
 
-    force = force_mesh(transducer_phases,points,norms,areas,board,BEM_forward_model_grad,params)
+    centre_of_mass = get_centre_of_mass_as_points(scatterer)
 
+    force = force_mesh(transducer_phases,points,norms,areas,board,grad_H,params)
+    torque = torque_mesh(transducer_phases,points,norms,areas,centre_of_mass,board,force=force)
+    weight = get_weight(scatterer)
 
-    print(force)
+    return (torch.sum(force,dim=[1,2]) + torch.sum(torque,dim=[1,2]) - weight)**2
+    
