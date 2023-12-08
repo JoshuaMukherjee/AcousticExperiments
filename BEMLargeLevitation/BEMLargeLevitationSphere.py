11eb8c14658769ba9153afe95a6464f072b18aaf
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-12-08 16:31:34 +0000

    loss

--- BEMLargeLevitation/BEMLargeLevitationSphere.py
+++ BEMLargeLevitation/BEMLargeLevitationSphere.py
@@ -1,20 +1,23 @@
-from BEMLevitationObjectives import BEM_levitation_objective
-from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, get_lines_from_plane
+from BEMLevitationObjectives import BEM_levitation_objective, sum_forces_torque,sum_top_bottom_force_torque
+from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, get_lines_from_plane, downsample, get_centre_of_mass_as_points
 from acoustools.Utilities import TRANSDUCERS, propagate_abs, get_convert_indexes
-from acoustools.BEM import compute_H, grad_H
+from acoustools.BEM import compute_H, grad_H, get_cache_or_compute_H_gradients, get_cache_or_compute_H,propagate_BEM_pressure
 from acoustools.Visualiser import Visualise
 from acoustools.Solvers import gradient_descent_solver, wgs_wrapper, wgs_batch
 from acoustools.Optimise.Constraints import constrain_phase_only
 from acoustools.Gorkov import force_mesh
 
-import torch
+import torch, vedo
 
 if __name__ == "__main__":
 
-    path = "Media/Sphere-lam1.stl"
+    path = "Media/Sphere-lam2.stl"
     scatterer = load_scatterer(path,dy=-0.06) #Make mesh at 0,0,0
     
     scale_to_diameter(scatterer,0.04)
+
+    # scatterer = downsample(scatterer,factor=5)
+
 
     print(scatterer.bounds())
     # x1,x2,_,_,_,_ = scatterer.bounds()
@@ -23,16 +26,10 @@ if __name__ == "__main__":
     board = TRANSDUCERS
 
     centres = get_centres_as_points(scatterer)
+    centre_of_mass = get_centre_of_mass_as_points(scatterer)
 
     H = compute_H(scatterer, board)
-    # Hx, Hy, Hz = grad_H(scatterer,board)
-
-    # x = wgs_wrapper(centres, A=H)
-
-    # print(propagate_abs(x,centres,A=H))
-
-    # x = wgs_wrapper(centres)
-
+ 
     norms = get_normals_as_points(scatterer)
     areas = get_areas(scatterer)
 
@@ -40,21 +37,49 @@ if __name__ == "__main__":
     # norms  =norms.expand((2,-1,-1))
     areas = areas.expand((1,-1,-1))
 
-    
+    Hx, Hy, Hz = get_cache_or_compute_H_gradients(scatterer, board,print_lines=True)
+    H = get_cache_or_compute_H(scatterer,board,print_lines=True)
 
-
+    top_board_idx = (centres[:,2,:] > centre_of_mass[:,2,:]) #For some reason indexes seem swapped, for now this works?
 
     params = {
         "scatterer":scatterer,
         "norms":norms,
         "areas":areas,
-        "weight":0.0027*9.81
+        "weight":-1*0.0027*9.81,
+        "Hgrad":(Hx,Hy,Hz),
+        "H":H,
+        "loss":sum_top_bottom_force_torque,
+        "loss_params":{
+              "top_board_idx":top_board_idx
+        }
     }
 
 
-    x = gradient_descent_solver(centres, BEM_levitation_objective,constrains=constrain_phase_only,objective_params=params,log=True,iters=400,lr=0.005)
-    f = force_mesh(x,centres,norms,areas,board,grad_H,grad_function_args={"scatterer":scatterer })
-    print(torch.sum(f,dim=2), 0.0027*9.81)
+    BASE_LR = 1e-3
+    MAX_LR = 1e-1
+    EPOCHS = 2000
+
+    scheduler = torch.optim.lr_scheduler.CyclicLR
+    scheduler_args = {
+        "max_lr":MAX_LR,
+        "base_lr":BASE_LR,
+        "cycle_momentum":False,
+        "step_size_up":100
+    }
+    
+
+
+    x = gradient_descent_solver(centres, BEM_levitation_objective,constrains=constrain_phase_only,objective_params=params,log=True,\
+                                iters=EPOCHS,lr=BASE_LR,scheduler=scheduler, scheduler_args=scheduler_args)
+    f = force_mesh(x,centres,norms,areas,board,grad_H,params,Ax=Hx, Ay=Hy, Az=Hz,F=H)
+    
+    force_z = f[:,2,:]
+    force_z_top =force_z[top_board_idx]
+    force_z_bottom = force_z[~top_board_idx]
+    
+    print(torch.sum((force_z_bottom)), params["weight"], torch.sum(force_z_top), torch.sum((force_z_bottom)) + params["weight"] + torch.sum(force_z_top) )
+
 
     A = torch.tensor((-0.07,0, 0.07))
     B = torch.tensor((0.07,0, 0.07))
@@ -67,9 +92,10 @@ if __name__ == "__main__":
 
    
 
-    # Visualise(A,B,C,x,colour_functions=[propagate_abs], add_lines_functions=[get_lines_from_plane],add_line_args=[line_params])
+    Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure], add_lines_functions=[get_lines_from_plane],add_line_args=[line_params],\
+              colour_function_args=[{"H":H,"scatterer":scatterer,"board":board}],vmax=9000)
     
-    # exit()
+    exit()
 
 
     FLIP_INDEXES = get_convert_indexes()
--- BEMLargeLevitation/BEMLevitationObjectives.py
+++ BEMLargeLevitation/BEMLevitationObjectives.py
@@ -14,14 +14,28 @@ def BEM_levitation_objective(transducer_phases, points, board, targets=None, **o
     norms = objective_params["norms"]
     areas = objective_params["areas"]
 
+    loss_function = objective_params["loss"]
+    
+    if "loss_params" in objective_params:
+        loss_params = objective_params["loss_params"]
+    else:
+        loss_params = {} 
+
     params = {
         "scatterer":scatterer
     }
 
     centre_of_mass = get_centre_of_mass_as_points(scatterer)
 
-    Hx, Hy, Hz = grad_H(points=points, transducers=board, **{"scatterer":scatterer })
-    H = compute_H(scatterer,board)
+    if "Hgrad" not in objective_params:
+        Hx, Hy, Hz = grad_H(None, transducers=board, **{"scatterer":scatterer })
+    else:
+        Hx, Hy, Hz = objective_params["Hgrad"]
+    
+    if "H" not in objective_params:
+        H = compute_H(scatterer,board)
+    else:
+        H = objective_params["H"]
 
     force = force_mesh(transducer_phases,points,norms,areas,board,grad_H,params,Ax=Hx, Ay=Hy, Az=Hz,F=H)
     torque = torque_mesh(transducer_phases,points,norms,areas,centre_of_mass,board,force=force)
@@ -35,5 +49,25 @@ def BEM_levitation_objective(transducer_phases, points, board, targets=None, **o
     force_y = force[:,1,:]
     force_z = force[:,2,:]
     
+    return loss_function(force_x, force_y, force_z, weight, torque, **loss_params)
+
+def sum_forces_torque(force_x, force_y, force_z, weight, torque, **params):
+
     return (torch.sum(force_z,dim=1) - weight)**2  + torch.sum(torque,dim=[1,2])**2 + torch.sum(force_x,dim=1)**2 + torch.sum(force_y,dim=1)**2
+
+
+def sum_top_bottom_force_torque(force_x, force_y, force_z, weight, torque, **params):
+    top_board_idx = params["top_board_idx"]
     
+    force_z_top =force_z[top_board_idx]
+    force_z_bottom = force_z[~top_board_idx]
+
+    # print("Z-W",torch.sum((force_z_bottom - weight)**2).unsqueeze_(0))
+    # print("T", torch.sum(torque**2,dim=[1,2]))
+    # print("X", torch.sum(force_x**2,dim=1))
+    # print("Y", torch.sum(force_y**2,dim=1))
+    # print("Z", torch.sum(force_z_top**2))
+
+    # print(torch.sum((force_z_bottom)), weight,torch.sum(force_z_top) )
+
+    return (torch.sum((force_z_bottom)) + (weight+torch.sum(force_z_top)) )**2 + torch.sum(torque**2,dim=[1,2]) + torch.sum(force_x**2,dim=1) + torch.sum(force_y**2,dim=1)  - 1e-2*(torch.sum((force_z_bottom))**2)
