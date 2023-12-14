999d22e2e358cfa4c829e3f342d10800ed81e5ad
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2023-12-14 11:12:53 +0000

    levitation

--- BEMLargeLevitation/BEMLargeLevitationSphere.py
+++ BEMLargeLevitation/BEMLargeLevitationSphere.py
@@ -1,7 +1,7 @@
-from BEMLevitationObjectives import BEM_levitation_objective, sum_forces_torque,sum_top_bottom_force_torque, max_magnitude_min_force, BEM_levitation_objective_top_bottom,balance, BEM_levitation_objective_subsample
-
+from BEMLevitationObjectives import BEM_levitation_objective, sum_forces_torque,sum_top_bottom_force_torque, max_magnitude_min_force,\
+     BEM_levitation_objective_top_bottom,balance, BEM_levitation_objective_subsample, balance_max_z
 from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, get_lines_from_plane, downsample, get_centre_of_mass_as_points
-from acoustools.Utilities import TRANSDUCERS, propagate_abs, get_convert_indexes, create_board, device, TOP_BOARD, BOTTOM_BOARD
+from acoustools.Utilities import TRANSDUCERS, propagate_abs, get_convert_indexes, create_board, device, TOP_BOARD, BOTTOM_BOARD, write_to_file
 from acoustools.BEM import compute_H, grad_H, get_cache_or_compute_H_gradients, get_cache_or_compute_H,propagate_BEM_pressure
 from acoustools.Visualiser import Visualise
 from acoustools.Solvers import gradient_descent_solver, wgs_wrapper, wgs_batch
@@ -14,7 +14,7 @@ import torch, vedo
 
 if __name__ == "__main__":
 
-    path = "Media/Sphere-lam2.stl"
+    path = "Media/Sphere-lam1.stl"
     scatterer = load_scatterer(path,dy=-0.06) #Make mesh at 0,0,0
     
     scale_to_diameter(scatterer,0.04)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     Hx, Hy, Hz = get_cache_or_compute_H_gradients(scatterer, board,print_lines=True)
     H = get_cache_or_compute_H(scatterer,board,print_lines=True)
 
-    indexes = get_indexes_subsample(10, centres)
+    # indexes = get_indexes_subsample(1700, centres)
 
     params = {
         "scatterer":scatterer,
@@ -44,11 +44,11 @@ if __name__ == "__main__":
         "weight":-1*0.0027*9.81,
         "Hgrad":(Hx, Hy, Hz),
         "H":H,
-        "loss":balance,
+        "loss":balance_max_z,
         "loss_params":{
-              "weights": [1,1,1,1,1]
+              "weights": [1,1,1,1,5]
         },
-        "indexes":indexes
+        # "indexes":indexes
     }
 
 
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     #scheduler=scheduler, scheduler_args=scheduler_args    
 
 
-    x = gradient_descent_solver(centres, BEM_levitation_objective_subsample,constrains=constrain_phase_only,objective_params=params,log=True,\
+    x = gradient_descent_solver(centres, BEM_levitation_objective,constrains=constrain_phase_only,objective_params=params,log=True,\
                                 iters=EPOCHS,lr=BASE_LR, optimiser=torch.optim.Adam, board=board)
 
 
@@ -77,6 +77,7 @@ if __name__ == "__main__":
     
     # print(torch.sum((force_z_bottom)).item(), params["weight"], torch.sum(force_z_top).item(), (torch.sum((force_z_bottom)) + params["weight"] + torch.sum(force_z_top)).item() )
     print(torch.sum((force_z)).item(), params["weight"], (torch.sum((force_z)) + params["weight"]).item() )
+    print(torch.sum(torch.abs(force_z)).item())
     print(torch.sum(force_x).item())
     print(torch.sum(force_y).item())
 
@@ -89,30 +90,11 @@ if __name__ == "__main__":
 
     line_params = {"scatterer":scatterer,"origin":origin,"normal":normal}
 
-    Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure,propagate_abs], add_lines_functions=[get_lines_from_plane,None],add_line_args=[line_params,{}],\
-              colour_function_args=[{"H":H,"scatterer":scatterer,"board":board},{"board":board}],vmax=9000)
+    # Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure,propagate_abs], add_lines_functions=[get_lines_from_plane,None],add_line_args=[line_params,{}],\
+            #   colour_function_args=[{"H":H,"scatterer":scatterer,"board":board},{"board":board}],vmax=9000)
 
-    # Visualise(A,B,C,x,colour_functions=[propagate_abs], add_lines_functions=[get_lines_from_plane],add_line_args=[line_params],\
-    #           colour_function_args=[{}],vmax=9000)
+
+    write_to_file(x,"./BEMLargeLevitation/Paths/spherelev.csv",1)
+
+
     
-
-
-
-    # FLIP_INDEXES = get_convert_indexes()
-    # row = torch.angle(x).squeeze_()
-    # row_flip = row[FLIP_INDEXES]
-
-
-    # num_frames = 1
-    # num_transducers = 512
-
-    # output_f = open("./BEMLargeLevitation/Paths/spherelev"+".csv","w")
-    # output_f.write(str(num_frames)+","+str(num_transducers)+"\n")
-    # for i,phase in enumerate(row_flip):
-    #             output_f.write(str(phase.item()))
-    #             if i < 511:
-    #                 output_f.write(",")
-    #             else:
-    #                 output_f.write("\n")
-
-    # output_f.close()
