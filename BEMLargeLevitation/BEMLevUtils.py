150df42ffc3b7fd9592cb30129f02fe4e39e52b3
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2023-12-14 11:13:11 +0000

    N param

--- BEMLargeLevitation/BEMLevUtils.py
+++ BEMLargeLevitation/BEMLevUtils.py
@@ -1,7 +1,6 @@
 import torch
 
 def get_indexes_subsample(N, centres):
-    N = 400
     indexes = torch.zeros(centres.shape[2]).to(bool)
     mask = torch.randperm(centres.shape[2])[:N]
     indexes[mask] = True
