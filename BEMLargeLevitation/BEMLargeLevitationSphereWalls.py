d222484e7eaa1ef75d0e3ca0750530f61c1a91fd
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2024-01-02 11:26:55 +0000

    masking

--- BEMLargeLevitation/BEMLargeLevitationSphereWalls.py
+++ BEMLargeLevitation/BEMLargeLevitationSphereWalls.py
@@ -2,7 +2,7 @@ from BEMLevitationObjectives import BEM_levitation_objective, sum_forces_torque,
      BEM_levitation_objective_top_bottom,balance, BEM_levitation_objective_subsample, balance_max_z, weight_force, balance_greater_z
 from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, get_lines_from_plane, downsample,\
       get_centre_of_mass_as_points, get_weight, load_multiple_scatterers, merge_scatterers
-from acoustools.Utilities import TRANSDUCERS, propagate_abs, get_convert_indexes, create_board, device, TOP_BOARD, BOTTOM_BOARD, write_to_file
+from acoustools.Utilities import TRANSDUCERS, propagate_abs, get_convert_indexes, create_board, device, TOP_BOARD, BOTTOM_BOARD, write_to_file, get_rows_in
 from acoustools.BEM import compute_H, grad_H, get_cache_or_compute_H_gradients, get_cache_or_compute_H,propagate_BEM_pressure
 from acoustools.Visualiser import Visualise
 from acoustools.Solvers import gradient_descent_solver, wgs_wrapper, wgs_batch
@@ -39,22 +39,8 @@ if __name__ == "__main__":
     scatterer_cells = get_centres_as_points(scatterer)
     ball_cells = get_centres_as_points(ball)
     
-    B = scatterer_cells.shape[0]
-    N_ball = ball_cells.shape[2]
-    N_scatt = scatterer_cells.shape[2]
-    ball_cells_hash = torch.zeros((B,N_ball))
-    scatterer_cells_hash = torch.zeros((B,N_scatt))
-
-    for i,batch in enumerate(ball_cells.mT):
-        for j, row in enumerate(batch):
-            ball_cells_hash[i,j] = hash(row)
+    mask = get_rows_in(scatterer_cells,ball_cells, expand=False)
     
-    for i,batch in enumerate(scatterer_cells.mT):
-        for j, row in enumerate(batch):
-            scatterer_cells_hash[i,j] = hash(row)
-
-    mask = torch.isin(scatterer_cells_hash,ball_cells_hash)
-
     # scale_to_diameter(scatterer,0.04)
 
     print(scatterer)
@@ -82,7 +68,7 @@ if __name__ == "__main__":
         "H":H,
         "loss":balance_greater_z,
         "loss_params":{
-              "weights": [1000,1,1,1,10,10]
+              "weights": [1,1,1,1,1,1]
         },
         "indexes":mask.squeeze_()
     }
