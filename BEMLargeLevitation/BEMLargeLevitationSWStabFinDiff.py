e6ca25454049dc9337a3481f5dfe2c98c4685e55
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2024-01-24 17:28:23 +0000

    weights

--- BEMLargeLevitation/BEMLargeLevitationSWStabFinDiff.py
+++ BEMLargeLevitation/BEMLargeLevitationSWStabFinDiff.py
@@ -1,13 +1,14 @@
 from BEMLevitationObjectives import BEM_levitation_objective_subsample_stability_fin_diff, balance_greater_z_stab_fin_diff, levitation_balance_greater_grad_torque, levitation_balance_mag_grad_torque
 
 from acoustools.Mesh import load_scatterer, scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas,\
-      get_centre_of_mass_as_points, get_weight, load_multiple_scatterers, merge_scatterers, get_lines_from_plane,get_plane
+      get_centre_of_mass_as_points, get_weight, load_multiple_scatterers, merge_scatterers, get_lines_from_plane,get_plane, scatterer_file_name
 from acoustools.Utilities import TRANSDUCERS, write_to_file, get_rows_in, propagate_abs
 from acoustools.BEM import grad_H, get_cache_or_compute_H_gradients, get_cache_or_compute_H,propagate_BEM_pressure, get_cache_or_compute_H_2_gradients
 from acoustools.Visualiser import Visualise, force_quiver
 from acoustools.Solvers import gradient_descent_solver
 from acoustools.Optimise.Constraints import constrain_phase_only
 from acoustools.Gorkov import force_mesh, get_force_mesh_along_axis,torque_mesh
+import acoustools.Constants as Constants
 
 from BEMLevUtils import get_H_for_fin_diffs
 
@@ -20,17 +21,23 @@ if __name__ == "__main__":
 
     diff = 0.0025
     board = TRANSDUCERS
-
+ 
     wall_paths = ["Media/flat-lam1.stl","Media/flat-lam1.stl"]
-    walls = load_multiple_scatterers(wall_paths,dxs=[-0.085,0.085],rotys=[90,-90]) #Make mesh at 0,0,0
+    walls = load_multiple_scatterers(wall_paths,dxs=[-0.175/2,0.175/2],rotys=[90,-90]) #Make mesh at 0,0,0
+    walls.scale((1,19/12,19/12),reset=True,origin =False)
+    walls.filename = scatterer_file_name(walls)
+    print(walls)
 
-    
-    
-    ball_path = "Media/Sphere-lam4.stl"
+
+    ball_path = "Media/Sphere-lam1.stl"
     ball = load_scatterer(ball_path,dy=-0.06) #Make mesh at 0,0,0
     scale_to_diameter(ball,0.02)
+    # scale_to_diameter(ball, Constants.R*2)
 
     scatterer = merge_scatterers(ball, walls)
+
+    # vedo.show(scatterer,axes=1)
+    # exit()
 
     # ball_points = scatterer.vertices[ball_ids]
     # ball_cells = scatterer.map_cells_to_points(ball_points)
@@ -59,12 +66,12 @@ if __name__ == "__main__":
 
     Hx, Hy, Hz = get_cache_or_compute_H_gradients(scatterer, board,print_lines=True)
     H = get_cache_or_compute_H(scatterer,board,print_lines=True)
-    Haa = get_cache_or_compute_H_2_gradients(scatterer, board,print_lines=True)
 
     # indexes = get_indexes_subsample(1700, centres)
  
     # weight = -1*0.0027*9.81
-    weight = -1*get_weight(ball)
+    # weight = -1*get_weight(ball)
+    weight = -1*(0.1/1000)*9.81
     # weight = -1*0.002*9.81
 
     Hss = []
@@ -72,7 +79,7 @@ if __name__ == "__main__":
     Hyss = []
     Hzss = []
 
-    SCALE = 100
+    SCALE = 10
     startX = torch.tensor([[-1*diff],[0],[0]])/SCALE
     endX = torch.tensor([[diff],[0],[0]])/SCALE
     Hs, Hxs, Hys, Hzs = get_H_for_fin_diffs(startX, endX, [ball.clone(),walls], board, steps=1, use_cache=True, print_lines=False)
@@ -105,8 +112,8 @@ if __name__ == "__main__":
         # "weight":-1*0.00100530964,
         "Hgrad":(Hx, Hy, Hz),
         "H":H,
-        "Hgrad2":Haa,
         "loss":levitation_balance_mag_grad_torque,
+        # "loss":levitation_balance_greater_grad_torque,
         "loss_params":{
             #   "weights": [1000,1,1,1,1,1,1e-17,1000,10000]
             # "weights": [1000,1,1,1,1,10,10,10,100,100]#ForceXYZFinDiff
@@ -121,7 +128,10 @@ if __name__ == "__main__":
             # 'weights':[1000000000,100,10000,0]
             # 'weights':[100,1,100,1000]
             # 'weights':[5,2,3,5]
-            'weights':[3,1,10,1]
+            # 'weights':[3,1,10,1]
+            # 'weights':[300,10,40000,5]
+            # 'weights':[450,2,-5,3]
+            'weights':[1,5,3,3]
         },
         "indexes":mask.squeeze_(),
         "diff":diff,
@@ -135,7 +145,7 @@ if __name__ == "__main__":
 
     BASE_LR = 1e-2
     MAX_LR = 1e-1
-    EPOCHS = 10
+    EPOCHS = 800
 
     scheduler = torch.optim.lr_scheduler.CyclicLR
     scheduler_args = {
@@ -164,7 +174,6 @@ if __name__ == "__main__":
     torque_z = torque[:,2,:][:,mask]
 
 
-
     
     # print(torch.sum((force_z_bottom)).item(), params["weight"], torch.sum(force_z_top).item(), (torch.sum((force_z_bottom)) + params["weight"] + torch.sum(force_z_top)).item() )
     print(torch.sum((force_z)).item(), params["weight"], (torch.sum((force_z)) + params["weight"]).item(), 1000 * (torch.sum((force_z)) + params["weight"]).item()/9.81)
@@ -172,12 +181,22 @@ if __name__ == "__main__":
     print(torch.sum(torch.abs(force_y)).item(), torch.sum(force_y).item(), torch.sum(torque_y).item())
     print(torch.sum(torch.abs(force_z)).item(), torch.sum(force_z).item() + params["weight"], torch.sum(torque_z).item())
     
+    print("Writing...")
+    write_to_file(x,"./BEMLargeLevitation/Paths/spherelev.csv",1)
+    print("File Written")
     # exit()
+
+    # A = torch.tensor((0,-0.09, 0.09))
+    # B = torch.tensor((0,0.09, 0.09))
+    # C = torch.tensor((0,-0.09, -0.09))
+    # normal = (1,0,0)
+    # origin = (0,0,0)
+
     A = torch.tensor((-0.09,0, 0.09))
     B = torch.tensor((0.09,0, 0.09))
     C = torch.tensor((-0.09,0, -0.09))
     normal = (0,1,0)
-    origin = (0,0,-0.07)
+    origin = (0,0,0)
 
     # A = torch.tensor((-0.07, 0.07,0))
     # B = torch.tensor((0.07, 0.07,0))
@@ -189,17 +208,14 @@ if __name__ == "__main__":
     line_params = {"scatterer":scatterer,"origin":origin,"normal":normal}
     line_params_wall = {"scatterer":walls,"origin":origin,"normal":normal}
 
-    # Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure,propagate_BEM_pressure], add_lines_functions=[get_lines_from_plane,get_lines_from_plane],add_line_args=[line_params,line_params_wall],\
-            #   colour_function_args=[{"H":H,"scatterer":scatterer,"board":board},{"board":board,"scatterer":walls}],vmax=9000, show=True)
+    Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure,propagate_BEM_pressure], add_lines_functions=[get_lines_from_plane,get_lines_from_plane],add_line_args=[line_params,line_params_wall],\
+              colour_function_args=[{"H":H,"scatterer":scatterer,"board":board},{"board":board,"scatterer":walls}],vmax=9000, show=True)
 
     # Visualise(A,B,C,x,colour_functions=[propagate_BEM_pressure,propagate_abs], add_lines_functions=[get_lines_from_plane,None],add_line_args=[line_params,{}],\
             #   colour_function_args=[{"H":H,"scatterer":scatterer,"board":board},{}],vmax=9000, show=True)
     # exit()
    
-    # print("Writing...")
-    # write_to_file(x,"./BEMLargeLevitation/Paths/spherelev.csv",1)
-    # print("File Written")
-    # exit()
+    
 
     pad = 0.005
     planar = get_plane(scatterer,origin,normal)
@@ -222,14 +238,16 @@ if __name__ == "__main__":
     startZ = torch.tensor([[0],[0],[-1*diff]])
     endZ = torch.tensor([[0],[0],[diff]])
     
+    # exit()
     
     steps = 60
     path = "Media"
-    print_lines = True
+    print_lines = False
     FxsX, FysX, FzsX = get_force_mesh_along_axis(startX, endX, x, [ball.clone(),walls], board,mask,steps=steps, use_cache=True, print_lines=print_lines,path=path)
     FxsY, FysY, FzsY = get_force_mesh_along_axis(startY, endY, x, [ball.clone(),walls], board,mask,steps=steps, use_cache=True, print_lines=print_lines,path=path)
     FxsZ, FysZ, FzsZ = get_force_mesh_along_axis(startZ, endZ, x, [ball.clone(),walls], board,mask,steps=steps, use_cache=True, print_lines=print_lines,path=path)
 
+    labs = ["X", "Y", "Z"]
     for i,(Fxs, Fys, Fzs) in enumerate([[FxsX, FysX, FzsX], [FxsY, FysY, FzsY], [FxsZ, FysZ, FzsZ] ]):
         Fxs = [f.cpu().detach().numpy() for f in Fxs]
         Fys = [f.cpu().detach().numpy() for f in Fys]
@@ -243,11 +261,13 @@ if __name__ == "__main__":
         plt.plot(Fxs, label="$F_x$")
         plt.plot(Fys, label="$F_y$")
         plt.plot(Fzs, label="$F_z-mg$")
-        plt.xlabel("Distance (mm)")
+        plt.xlabel("$\Delta$" + labs[i] + " (mm)")
         plt.xticks(xticks, xticklabs)
         plt.ylabel("Restoring Force")
-    plt.legend()
+        if i == 0: plt.legend()
+    plt.tight_layout()
     plt.show()
+
 
 
 
