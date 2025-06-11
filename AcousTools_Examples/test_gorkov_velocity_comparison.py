53708d15572ec1dd26a542c503e2fe812a11209b
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2025-06-11 18:04:39 +0100

    velocity

--- AcousTools_Examples/test_gorkov_velocity_comparison.py
+++ AcousTools_Examples/test_gorkov_velocity_comparison.py
@@ -23,15 +23,34 @@ def gorkov_analytical_velocity(activations: Tensor, points: Tensor,board:Tensor|
     pz = (Fz@activations).squeeze(2).unsqueeze(0)
 
     grad  = torch.cat((px,py,pz),dim=1)
+    print(grad)
+    exit()
+
+    K1, K2 = get_gorkov_constants(V=V)
+    g = K2*(torch.sum(torch.abs(grad)**2, dim=1))
+
     velocity = grad /( 1j * c.p_0 * c.f)
     velocity_time_average = 1/2 * torch.sum(velocity * velocity.conj().resolve_conj(), dim=1, keepdim=True).real
 
-    f1 = 1- (c.p_0*c.c_0**2)/(c.p_p * c.c_p**2)
-    f2 = 2 * (c.p_0 - c.p_p)/(2*c.p_p + c.p_0)
-    
+    f1 = 1 - (c.p_0*c.c_0**2)/(c.p_p * c.c_p**2)
+    f2 = 2 * (c.p_p - c.p_0)/(2*c.p_p + c.p_0)
 
+
+    print((2 * c.pi* c.radius**3 *f2*c.p_0/2 * velocity_time_average)/ g, sep='\n')
+    print()
+    # exit() 
+    #  This is too big by factor of 91.1250
+
+    
     U = 2 * c.pi* c.radius**3 * ((f1/(3*c.p_0*c.c_0**2) * 1/2*pressure_square) - f2*c.p_0/2 * velocity_time_average )
     return U.permute(0,2,1)
+
+def gorkov_ratio(activations: Tensor, points: Tensor,board:Tensor|None=None, axis:str="XYZ", V:float=c.V, **params) -> Tensor:
+    U = gorkov_analytical(activations, points, board)
+    U_v = gorkov_analytical_velocity(activations, points, board)
+    return U / U_v
+
+
 
 board = TRANSDUCERS
 
@@ -39,7 +58,8 @@ p = create_points(1,1)
 x = kd_solver(p, board=board)
 x = add_lev_sig(x)
 
-Visualise(*ABC(0.05, origin=p), x, points=p, 
-          colour_functions=[propagate_abs, gorkov_analytical, gorkov_analytical_velocity], 
-          colour_function_args=[{'board':board}, {'board':board}, {'board':board}],
-          link_ax=None)
+r = 100
+Visualise(*ABC(0.02, origin=p), x, points=p, 
+          colour_functions=[propagate_abs, gorkov_analytical, gorkov_analytical_velocity, gorkov_ratio], 
+          colour_function_args=[{'board':board}, {'board':board}, {'board':board},{'board':board}],
+          link_ax=None, res = (r,r))
