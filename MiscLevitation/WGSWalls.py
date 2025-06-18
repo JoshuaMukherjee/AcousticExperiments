4b47a126a79efd5b1de612185c17425b9694702a
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2025-06-18 17:13:05 +0100

    walls

--- MiscLevitation/WGSWalls.py
+++ MiscLevitation/WGSWalls.py
@@ -1,5 +1,5 @@
 from acoustools.Utilities import create_points,add_lev_sig, TRANSDUCERS, propagate_abs
-from acoustools.Solvers import wgs_wrapper
+from acoustools.Solvers import wgs
 from acoustools.Visualiser import Visualise
 
 from acoustools.Mesh import load_multiple_scatterers, get_lines_from_plane
@@ -9,17 +9,18 @@ import torch
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    p = create_points(4,y=0)
+    p = create_points(1,y=0,x=0.04)
 
     # print(p.shape)
-    x = wgs_wrapper(p)
+    x = wgs(p)
     x = add_lev_sig(x)
 
-    wall_paths = ["Media/flat-lam1.stl","Media/flat-lam1.stl"]
+    path = '../BEMMedia'
+    wall_paths = ["../BEMMedia/flat-lam1.stl","../BEMMedia/flat-lam1.stl"]
     walls = load_multiple_scatterers(wall_paths,dxs=[-0.06,0.06],rotys=[90,-90]) #Make mesh at 0,0,0
-    H = get_cache_or_compute_H(walls, TRANSDUCERS)
-    E = compute_E(walls, p, TRANSDUCERS,H=H)
-    x_BEM = wgs_wrapper(p, A=E)
+    H = get_cache_or_compute_H(walls, TRANSDUCERS, path=path)
+    E = compute_E(walls, p, TRANSDUCERS,H=H, path=path)
+    x_BEM = wgs(p, A=E)
     x_BEM = add_lev_sig(x_BEM)
 
 
@@ -32,6 +33,7 @@ if __name__ == "__main__":
     # Visualise(A,B,C, x, p,vmax=9000)
 
     line_params = {"scatterer":walls,"origin":origin,"normal":normal}
-    Visualise(A,B,C,x_BEM, p,add_lines_functions=[get_lines_from_plane, get_lines_from_plane],add_line_args=[line_params,line_params], colour_functions=[propagate_BEM_pressure,propagate_abs], colour_function_args=[{"H":H,"scatterer":walls,"board":TRANSDUCERS},{}],vmax=9000)
+    Visualise(A,B,C,x_BEM, p, colour_functions=[propagate_BEM_pressure,propagate_abs], colour_function_args=[{"H":H,"scatterer":walls,"board":TRANSDUCERS},{}],vmax=9000)
 
 
+# MiscLevitation/WGSWalls.py
