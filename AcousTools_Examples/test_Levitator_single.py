5ed960777493e7c679d2571addd79ef5836a6107
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2025-08-13 09:40:42 +0100

    reflector + id

--- AcousTools_Examples/test_Levitator_single.py
+++ AcousTools_Examples/test_Levitator_single.py
@@ -1,18 +1,20 @@
 from acoustools.Levitator import LevitatorController
 
-from acoustools.Utilities import BOTTOM_BOARD, create_points, propagate_abs
+from acoustools.Utilities import BOTTOM_BOARD, create_points, propagate_abs, TOP_BOARD
 from acoustools.Solvers import wgs
+
+board = TOP_BOARD
 
 mat_to_world = (1, 0, 0, 0,
                 0, 1, 0, 0,
                 0, 0, 1, 0,
                 0, 0, 0, 1)
 
-lev = LevitatorController(ids=(73,), matBoardToWorld=mat_to_world)
+lev = LevitatorController(ids=(53,), matBoardToWorld=mat_to_world)
 
 p = create_points(1,1,x=0,y=0,z=0)
-x = wgs(p, board = BOTTOM_BOARD)
-print(propagate_abs(x, p, board = BOTTOM_BOARD))
+x = wgs(p, board = board) *0.6
+print(propagate_abs(x, p, board = board))
 
 
 
