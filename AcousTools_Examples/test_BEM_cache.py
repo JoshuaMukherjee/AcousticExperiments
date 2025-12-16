4d093c838dd75a8d2adfeec5967494f407a2ddef
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2025-12-16 14:47:27 +0000

    cache

--- AcousTools_Examples/test_BEM_cache.py
+++ AcousTools_Examples/test_BEM_cache.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     from acoustools.Utilities import add_lev_sig, TRANSDUCERS
-    from acoustools.Solvers import wgs_wrapper
+    from acoustools.Solvers import wgs
     from acoustools.Mesh import load_multiple_scatterers, get_normals_as_points, get_centres_as_points, get_weight, scale_to_diameter, get_centre_of_mass_as_points,get_lines_from_plane
     from acoustools.BEM import compute_E, propagate_BEM_pressure
     import acoustools.Constants as c 
-    from acoustools.Visualiser import Visualise
+    from acoustools.Visualiser import Visualise, ABC
 
     import vedo, torch
 
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     E, F,G,H = compute_E(scatterer, com, TRANSDUCERS, return_components=True, path=path, print_lines=True)
 
-    x = wgs_wrapper(com, board=board, A=E)
+    x = wgs(com, board=board, A=E)
     x = add_lev_sig(x)
 
     print(propagate_BEM_pressure(x, com, scatterer, board=TRANSDUCERS, use_cache_H=True, path=path, print_lines=True))
@@ -36,12 +36,14 @@ if __name__ == "__main__":
     print(propagate_BEM_pressure(x, com, scatterer, board=TRANSDUCERS, use_cache_H=False, path=path, print_lines=True))
 
 
-    A = torch.tensor((-0.01,0, 0.01))
-    B = torch.tensor((0.01,0, 0.01))
-    C = torch.tensor((-0.01,0, -0.01))
     normal = (0,1,0)
     origin = (0,0,0)
 
 
     line_params = {"scatterer":scatterer,"origin":origin,"normal":normal}
-    Visualise(A,B,C, x, colour_functions=[propagate_BEM_pressure, propagate_BEM_pressure],colour_function_args=[{"scatterer":scatterer,"board":TRANSDUCERS,"path":path}, {"scatterer":scatterer,"board":TRANSDUCERS,"path":path,"use_cache_H":False}],vmax=9000, show=True,add_lines_functions=[get_lines_from_plane, get_lines_from_plane], add_line_args=[line_params,line_params])
+    Visualise(*ABC(0.01), x, colour_functions=[propagate_BEM_pressure, propagate_BEM_pressure, '-'],
+                colour_function_args=[{"scatterer":scatterer,"board":TRANSDUCERS,"path":path, 'internal_points':com}, 
+                                    {"scatterer":scatterer,"board":TRANSDUCERS,"path":path,"use_cache_H":False, 'internal_points':com}],
+                                    vmax=9000, show=True, link_ax=None,
+                # add_lines_functions=[get_lines_from_plane, get_lines_from_plane], add_line_args=[line_params,line_params]
+                )
