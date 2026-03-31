2c844e69ada014a567aca67a24fc5f220b6255a3
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2026-03-31 16:17:21 +0100

    circle

--- AcousTools_Examples/test_Levitator_circle.py
+++ AcousTools_Examples/test_Levitator_circle.py
@@ -1,36 +1,42 @@
 if __name__ == '__main__':
     from acoustools.Levitator import LevitatorController
-    from acoustools.Utilities import create_points, add_lev_sig, write_to_file
+    from acoustools.Utilities import create_points, add_lev_sig
     from acoustools.Solvers import wgs
+    from acoustools.Export.Holo import save_holograms, load_holograms
     import math, time
 
-    print('Computing...')
-    xs = []
-    poss=[]
+    COMPUTE = False
     N = 250
-    radius = 0.02
-    for i in range(N):
-        t = ((3.1415926*2) / N) * i
-        x = radius * math.sin(t)
-        z = radius * math.cos(t)
-        print(i,x,0,z)
-        poss.append((x,0,z))
-        p = create_points(1,1,x=x,y=0,z=z)
-        x = wgs(p)
-        x = add_lev_sig(x)
-        xs.append(x)
-    print('Finished\nConnecting...')
+    if COMPUTE:
+        print('Computing...')
+        xs = []
+        poss=[]
+        
+        radius = 0.02
+        for i in range(N):
+            t = ((3.1415926*2) / N) * i
+            x = radius * math.sin(t)
+            y = radius * math.cos(t)
+            print(i,x,y,0)
+            poss.append((x,y,0))
+            p = create_points(1,1,x=x,y=y,z=0)
+            x = wgs(p)
+            x = add_lev_sig(x)
+            xs.append(x)
+        print('Finished\nConnecting...')
+        save_holograms(xs,f'Circle-{N}.holo')
+    else:
+        xs = load_holograms(f'Circle-{N}.holo')
 
-    lev = LevitatorController(ids=(999,1000))
+    lev = LevitatorController()
 
     lev.levitate(xs[0])
-    print(poss[0])
     input()
     print('Moving...')
     try:
         while True:
             for x in xs:
-                lev.levitate(x)
+                lev.levitate(0.35*x)
                 time.sleep(0.1)
     except:  
         print('Finished')
